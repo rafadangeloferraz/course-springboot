a4e9c3888e5dd93f75c9bbdde6b5b00f883b1db0
Rafael DAngelo Ferraz <rafadangeloferraz@gmail.com>
2022-09-22 22:34:23 -0300

    Exception handling - update

--- src/main/java/com/duquesantana/course/resourcers/UserResource.java
+++ src/main/java/com/duquesantana/course/resourcers/UserResource.java
@@ -1,13 +1,19 @@
 package com.duquesantana.course.resourcers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.duquesantana.course.entities.User;
 import com.duquesantana.course.services.UserService;
@@ -34,5 +40,27 @@ public class UserResource {//UserResource depende do UserService assim necessita
 		return ResponseEntity.ok().body(obj);
 	}
 	
-
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) { //@RequestBody para dizer q obj User vai chegar no modo json na hora de fazer requisição e esse json vai ser deserealizado para um objeto user no java
+		obj = service.insert(obj);
+		//objeto uri contem o cabeçalho contendo o endereço do novo recurso inserido 
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+		
+		//return ResponseEntity.ok().body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 }
